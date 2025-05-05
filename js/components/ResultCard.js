85d15342225032fd017555ef0f1e6299c2c7dff0
Minhazul Abedin <115995410+OkayAbedin@users.noreply.github.com>
2025-05-05 23:14:34 +0600

    fix: Remove enrollment semester determination logic from student ID

--- js/components/ResultCard.js
+++ js/components/ResultCard.js
@@ -284,50 +284,6 @@ class ResultCard {
                     const logoSrc = logo.src;
                     logo.src = "assets/img/diu-logo.svg"; // Use the actual DIU logo from assets
 
-                    // Try to determine enrollment semester from student ID if possible
-                    if (studentInfo && studentInfo.studentId) {
-                        try {
-                            // Validate student ID format (XXX-YY-ZZZZ)
-                            const idRegex = /^(\d{3})-(\d{2})-(\d+)$/;
-                            const match = studentInfo.studentId.match(idRegex);
-                            
-                            if (match) {
-                                // Extract the parts from the ID
-                                const firstPart = match[1]; // First three digits
-                                
-                                // First digit is semester code (1=Spring, 2=Summer, 3=Fall)
-                                const semesterCode = firstPart.charAt(0);
-                                // Last two digits are the year (e.g., 22 for 2022)
-                                const yearCode = firstPart.substring(1);
-                                
-                                let enrollmentSemester = "";
-                                
-                                // Convert semester code to name
-                                if (semesterCode === '1') {
-                                    enrollmentSemester = "Spring ";
-                                } else if (semesterCode === '2') {
-                                    enrollmentSemester = "Summer ";
-                                } else if (semesterCode === '3') {
-                                    enrollmentSemester = "Fall ";
-                                }
-                                
-                                // Add year (20xx format)
-                                if (yearCode && yearCode.length === 2) {
-                                    enrollmentSemester += "20" + yearCode;
-                                }
-                                
-                                // Set enrollment semester if we determined it
-                                if (enrollmentSemester) {
-                                    studentInfo.enrollmentSession = enrollmentSemester;
-                                }
-                            } else {
-                                console.warn("Student ID does not match expected format (XXX-YY-ZZZZ):", studentInfo.studentId);
-                            }
-                        } catch (error) {
-                            console.warn("Could not determine enrollment semester from ID:", error);
-                        }
-                    }
-
                     // Add university address
                     const address = previewWindow.document.createElement("div");
                     address.style.fontSize = "9pt";
